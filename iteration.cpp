ceab3b33bbaa2c074684bcd1db49836c90b0c8c2
Nikita Ardashev <nikitaardashev2004@yandex.ru>
2023-12-23 04:33:17 +0300

    Errors

--- iteration.cpp
+++ iteration.cpp
@@ -32,9 +32,21 @@ VectorXd solveGaussSeidel(const MatrixXd& A, const VectorXd& b, int maxIteration
     return x;
 }
 
-VectorXd solveSimpleIteration(const MatrixXd& A, const VectorXd& b, int maxIterations, double tolerance) {
+struct IterativeResult {
+    VectorXd solution;
+    int iterations;
+    double priorErrorEstimate;
+    double posteriorErrorEstimate;
+    double actualError;
+};
+
+IterativeResult solveSimpleIteration(const MatrixXd& A, const VectorXd& b, int maxIterations, double tolerance) {
     int n = A.rows();
     VectorXd x = VectorXd::Zero(n);
+
+    IterativeResult result;
+    result.iterations = 0;
+    result.priorErrorEstimate = 0.0;
 
     for (int k = 0; k < maxIterations; ++k) {
         VectorXd x_new = x;
@@ -43,20 +55,34 @@ VectorXd solveSimpleIteration(const MatrixXd& A, const VectorXd& b, int maxItera
             double sum = 0.0;
             for (int j = 0; j < n; ++j) {
                 if (j != i) {
-                    sum += A(i, j) * x(j);
+                    sum += A(i, j) * x_new(j);
                 }
             }
             x(i) = (b(i) - sum) / A(i, i);
         }
 
+        // Calculate a priori error estimate (prior to convergence check)
+        if (k == 0) {
+            result.priorErrorEstimate = (x - x_new).norm();
+        }
+
         // Check for convergence
         if ((x - x_new).norm() < tolerance) {
             cout << "Converged in " << k + 1 << " iterations." << endl;
+            result.iterations = k + 1;
             break;
         }
     }
 
-    return x;
+    // Calculate a posteriori error estimate
+    result.posteriorErrorEstimate = (A * x - b).norm();
+
+    // Calculate actual error
+    result.actualError = (x - A.lu().solve(b)).norm();
+
+    result.solution = x;
+
+    return result;
 }
 
 int main() {
@@ -73,13 +99,19 @@ int main() {
 
     // Solve the system using custom Gauss-Seidel method
     VectorXd zeidelSolution = solveGaussSeidel(A, b, 1000, 1e-6);
-    VectorXd simpleSolution = solveSimpleIteration(A, b, 1000, 1e-6);
+    IterativeResult result = solveSimpleIteration(A, b, 1000, 1e-6);
+
+    // Print the results
+    cout << "Solution:\n" << result.solution << endl;
+    cout << "Number of iterations: " << result.iterations << endl;
+    cout << "A priori error estimate: " << result.priorErrorEstimate << endl;
+    cout << "A posteriori error estimate: " << result.posteriorErrorEstimate << endl;
+    cout << "Actual error: " << result.actualError << endl;
 
     VectorXd eigenSolution = A.lu().solve(b);
 
     // Print the solutions
     cout << "Custom Gauss-Seidel Solution:\n" << zeidelSolution << endl;
-    cout << "Simple iteration Solution:\n" << simpleSolution << endl;
     cout << "Eigen LU Solution:\n" << eigenSolution << endl;
 
     return 0;
